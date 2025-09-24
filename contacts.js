5d3cd7c82088cdf25537fc760a7ef6350f70e4b6
Zack Brashear <zackbrashear@gmail.com>
2025-09-24 11:28:19 -0400

    final version. Added session and store plus flash

--- contacts.js
+++ contacts.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const { body, validationResult } = require("express-validator");
-const app = express();
+const session = require("express-session");
+const store = require("connect-loki");
+const flash = require("express-flash");
 
-let contactData = [
+const app = express();
+const LokiStore = store(session);
+
+const contactData = [
   {
     firstName: "Mike",
     lastName: "Jones",
@@ -42,6 +47,10 @@ const sortContacts = contacts => {
   });
 };
 
+const clone = object => {
+  return JSON.parse(JSON.stringify(object));
+};
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -49,13 +58,42 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 app.use(morgan("common"));
 
+app.use(session({
+  cookie: {
+    httpOnly: true,
+    maxAge: 31 * 24 * 60 * 60 * 1000,
+    path: "/",
+    secure: false,
+  },
+  name: "launch-school-contacts-manager-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+  store: new LokiStore({}),
+}))
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  if (!("contactData" in req.session)) {
+    req.session.contactData = clone(contactData);
+  }
+  next();
+})
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash
+  next();
+})
+
 app.get("/", (req, res) => {
   res.redirect("/contacts");
 });
 
 app.get("/contacts", (req, res) => {
   res.render("contacts", {
-    contacts: sortContacts(contactData),
+    contacts: sortContacts(req.session.contactData),
   });
 });
 
@@ -96,23 +134,25 @@ app.post("/contacts/new",
   (req, res, next) => {
     let errors = validationResult(req)
     if (!errors.isEmpty()) {
+      errors.array().forEach(err => req.flash("error", err.msg));
+
       res.render("new-contact", {
+        flash: req.flash(),
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         phoneNumner: req.body.phoneNumber,
-        errorMessages: errors.array().map(error => error.msg),
       });
     } else {
       next();
     }
   },
   (req, res) => {
-    contactData.push({
+    req.session.contactData.push({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       phoneNumber: req.body.phoneNumber,
     });
-
+    req.flash("success", "New contact created!")
     res.redirect("/contacts");
   }
 );
